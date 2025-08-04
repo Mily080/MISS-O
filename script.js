9ca3672b663daba87123e4b790e4cbb725136e8a
Kemilyy <kemily.santana@escola.pr.gov.br>
2025-08-04 12:24:03 -0300

    feito

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativa");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
+const Perguntas = [
+    {
+        enunciado: a pesca é desde quando
+        caixaAlternativas: [
+            
+        ]
+    }
+]
+]
